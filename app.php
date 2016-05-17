7679eccf97a72e34abc932c493ba404cde92baea
By-Vasiliy <vasilyuk.vasiliy@gmail.com>
2016-05-17 20:08:54 +0300

    Update, change action logic

--- app.php
+++ app.php
@@ -17,7 +17,7 @@
 define('GA_SSL_URL', 'https://ssl.google-analytics.com/collect');
 define('SES_COOKIE', '__SID');
 define('SES_LIFETIME', 2*365*24*60*60); // 2 Year
-define('RED_URL', 'https://git.io/vrsqr'); //Redirect url from bad request
+define('RED_URL', 'https://git.io/vrsil'); //Redirect url from bad request
 define('DEFAULT_PRINT_IMG_TYPE', 'png'); // or gif, or jpg
 define('IMG_CONF_JSON','img/config.json');
 
@@ -50,11 +50,12 @@ header('Expires: ' . date('r'));
 if($_SERVER['REQUEST_METHOD']!='GET'){
 	header('HTTP/1.1 405 Method Not Allowed');
 	die('Method Not Allowed');
-} $_POST=NULL; $_REQUEST=NULL;
+} $_POST=NULL; $_REQUEST=NULL; $r = false;
 $_GET = array_change_key_case($_GET, CASE_LOWER); //All key 
 // Session start
 session_start();
 
+//exit(print_r($_SERVER,true));
 
 // Favicon.ico
 if($_SERVER['REQUEST_URI']=='/favicon.ico'){
@@ -68,20 +69,19 @@ if($_SERVER['REQUEST_URI']=='/favicon.ico'){
 }
 
 
-// Router flag
-$r = false;
-// Pars url
-preg_match_all('/([A-Z]{2}-[0-9]{1,}-[0-9]{1,})([^?]*)?.*/', $_SERVER['REQUEST_URI'], $uRL);
-// Cheking
-if( isset($uRL[1][0]) ){
-	$gAReq['tid']=$uRL[1][0]; //The tracking ID / web property ID. The format is UA-XXXX-Y.
-	$r=true;
+// Tracking
+$cheak = stripos($_SERVER['REQUEST_URI'], '/t');
+if($cheak !== false && $cheak == 0){
+	if( !empty($_GET['gtid']) ) gaDataSend(); // If is set google tracking ID
+	if( !empty($_GET['go']) ){
+		$url = urlValidator($_GET['go']);
+		if($url){ header('Location: '.$url); exit();}
+			else header('Location: '.RED_URL);
+	}
+	checkAndPrintImgList();
+	exit();
 }
 
-
-// URL access img collecton if no tracking / testing images paranetrs
-preg_match("/^\/image-collection[\/]{0,1}[?]{0,1}.*$/", $_SERVER['REQUEST_URI'], $ic);
-if($ic){checkAndPrintImgList();	exit();}
 
 // If an invalid request redirects users
 if(!$r){
@@ -91,52 +91,9 @@ if(!$r){
 }
 
 
-// Set GA data
-if( isset($_SERVER['HTTP_USER_AGENT']) ) $gAReq['ua']=$_SERVER['HTTP_USER_AGENT']; // User agent
-if( httpUserLang() ) $gAReq['ul']=httpUserLang(); //User lang
-if( isset($_SERVER['HTTP_REFERER']) ) $gAReq['dr']=$_SERVER['HTTP_REFERER']; //Document Referrer
-if( httpUserIp() ) $gAReq['uip']=httpUserIp(); //User IP
-if( !empty($uRL[2][0]) ) $gAReq['dp']=$uRL[2][0];
-	else $gAReq['dp']= '/'; //Site page URI
-
-// Use referer / request uri +@+ referer
-if( isset($_SERVER['HTTP_REFERER'], $_GET['ur']) ){
-	$uri = $_SERVER['HTTP_REFERER'];
-	$uri = str_replace(array('http://', 'https://'),'',$uri);
-	$gAReq['dp']=$uRL[2][0].'@'.$uri;
-}
-$gAReq['v']=1; //The Protocol version. The current value is '1'.
-$gAReq['t']='pageview'; //Hit type
-$gAReq['ds']='web' ; //Indicates the data source of the hit.
-$gAReq['qt']=700; //Transmission data delay ms
-$gAReq['cid']=$_COOKIE[SES_COOKIE]; //Client ID
-$gAReq['sc']='start'; //Used to control the session duration.
-
-
-
-$gAReq['z']=randInt(); // Random integer / clear cache
-
-
-// Generate query string
-$gAReqSrt = GA_SSL_URL.'?'.http_build_query($gAReq);
-// Send data to GA
-file_get_contents($gAReqSrt);
-
-if( isset($_GET['go']) ){
-	if(!empty($_GET['go'])){
-    $url = urlValidator($_GET['go']);
-    if($url){ header('Location: '.$url); exit();}
-	}
-	header('Location: '.RED_URL);
-	exit();
-}
-
-
-checkAndPrintImgList();
-exit();
 
 # Functions block
-//Validate and return image
+// Validate and return image
 function checkAndPrintImgList(){
 	$imgList = array();
 	if(file_exists(IMG_CONF_JSON)){
@@ -244,22 +201,48 @@ function httpUserIp(){
 	return $ip;
 }
 
+
 // GO / Validate url
 function urlValidator($url){
-	$url=trim($url);
-	$arUrl = parse_url($url);
-	$url = null;
-	if(!array_key_exists('scheme', $arUrl) 
-		|| !in_array($arUrl['scheme'], array('http', 'https')))
-			$arUrl['scheme'] = 'http';
-	if(array_key_exists('host', $arUrl) &&
-			!empty($arUrl['host']))
-				$url = sprintf('%s://%s%s', $arUrl['scheme'],
-					$arUrl['host'], $arUrl['path']);
-	else if(preg_match('/^\w+\.[\w\.]+(\/.*)?$/', $arUrl['path']))
-					$url = sprintf('%s://%s', $arUrl['scheme'], $arUrl['path']);
-	if($url && empty($url['query']))
-			$url .= sprintf('?%s', $arUrl['query']);
-	if(substr($url, -1)=='?') $url=substr($url,0,-1);
-	return $url;
+	$url = preg_grep("/((https?|ftp):\/\/(\S*?\.\S*?))([\s)\[\]{},;\"\':<]|\.\s|$)/i", explode("\n", $url));
+	if($url) return $url[0];
+		else return false;
+}
+
+
+function gaDataSend(){
+	// Set GA data
+	if( empty($_GET['gtid']) ) return false;
+	if( isset($_SERVER['HTTP_USER_AGENT']) ) $gAReq['ua']=$_SERVER['HTTP_USER_AGENT']; // User agent
+	if( httpUserLang() ) $gAReq['ul']=httpUserLang(); //User lang
+	if( isset($_SERVER['HTTP_REFERER']) ) $gAReq['dr']=$_SERVER['HTTP_REFERER']; //Document Referrer
+	if( httpUserIp() ) $gAReq['uip']=httpUserIp(); //User IP
+
+	//Request uri
+	preg_match("/^([^?]*)?.*$/", $_SERVER['REQUEST_URI'], $uri);
+	if($uri[1] == '') $uri[1] = '/';
+	$gAReq['dp'] = trim($uri[1]); // Request uri
+	if(substr($gAReq['dp'], -1) != '/') $gAReq['dp'] .= '/';
+	// Use referer / request uri +@+ referer
+	if( isset($_SERVER['HTTP_REFERER'], $_GET['ur']) ){
+		$uri = $_SERVER['HTTP_REFERER'];
+		$uri = str_replace(array('http://', 'https://'),'',$uri);
+		$gAReq['dp'].='(REFERER)/'.$uri;
+	}$uri == null;
+
+	$gAReq['v']=1; //The Protocol version. The current value is '1'.
+	$gAReq['t']='pageview'; //Hit type
+	$gAReq['ds']='web' ; //Indicates the data source of the hit.
+	$gAReq['qt']=700; //Transmission data delay ms
+	$gAReq['cid']=$_COOKIE[SES_COOKIE]; //Client ID
+	$gAReq['sc']='start'; //Used to control the session duration.
+	$gAReq['tid']=$_GET['gtid'];
+	$gAReq['z']=randInt(); // Random integer / clear cache
+
+
+	// Generate query string
+	$gAReqSrt = GA_SSL_URL.'?'.http_build_query($gAReq);
+	// Send data to GA
+	file_get_contents($gAReqSrt);
+	return true;
 }
